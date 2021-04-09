ccddffe9f2e25ca09c3e9daaa68140b1bcebaa4b
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-04-09 14:37:44 +0200

    feat: configuration and validations

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pipeline-filebeat-logs
 Pipeline Logging using Filebeat Plugin
+
+This plugins replaces the default logs storage for pipelines,
+the new implementation stores the logs in Elasticsearch using Filebeat.
+
+# Requirements
+
+The plugin requires a Filebeat service up and running to connect to it.
+This Filebeat service should expose an input of one of the following types:
+
+[unix](https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-input-unix.html)
+[log](https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-input-log.html)
+[filestream](https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-input-filestream.html)
+[tcp](https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-input-tcp.html)
+[udp](https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-input-udp.html)
+
+The plugin will use this input to send the events.
--- src/main/java/io/jenkins/plugins/pipeline_filebeat_logs/FilebeatConfiguration.java
+++ src/main/java/io/jenkins/plugins/pipeline_filebeat_logs/FilebeatConfiguration.java
@@ -1,18 +1,247 @@
 package io.jenkins.plugins.pipeline_filebeat_logs;
 
+import com.cloudbees.plugins.credentials.Credentials;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.plugins.credentials.common.IdCredentials;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
+import hudson.Util;
+import hudson.model.Item;
+import hudson.security.ACL;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
 import jenkins.model.GlobalConfiguration;
+import jenkins.model.Jenkins;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
-import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
+/**
+ * Stores the configuration of the Pipeline Filebeat plugin.
+ */
 @Symbol("filebeatLogs")
 @Extension
 public class FilebeatConfiguration extends GlobalConfiguration {
+  private static final String ERROR_MALFORMED_URL = "The url is malformed.";
+  private static final List<String> validSchemas = Arrays.asList("unix", "tcp", "udp", "file");
 
-    @Nonnull
-    @Override
-    public String getDisplayName() {
-        return "Filebeat Logs settings";
+  @CheckForNull
+  private String kibanaUrl;
+  @CheckForNull
+  private String elasticsearchUrl;
+  @CheckForNull
+  private String input;
+  @CheckForNull
+  private String credentialsId;
+  @CheckForNull
+  private String indexPattern = "filebeat-*";
+
+  @NonNull
+  @Override
+  public String getDisplayName() {
+    return "Filebeat Logs settings";
+  }
+
+  @CheckForNull
+  public String getKibanaUrl() {
+    return kibanaUrl;
+  }
+
+  @DataBoundSetter
+  public void setKibanaUrl(String kibanaUrl) throws MalformedURLException {
+    this.kibanaUrl = Util.fixNull(kibanaUrl);
+  }
+
+  @CheckForNull
+  public String getInput() {
+    return input;
+  }
+
+  @DataBoundSetter
+  public void setInput(String input) {
+    this.input = Util.fixNull(input);
+  }
+
+  @CheckForNull
+  public String getElasticsearchUrl() {
+    return elasticsearchUrl;
+  }
+
+
+  @DataBoundSetter
+  public void setElasticsearchUrl(@CheckForNull String elasticsearchUrl) throws MalformedURLException {
+    this.elasticsearchUrl = Util.fixNull(elasticsearchUrl);
+  }
+
+  @CheckForNull
+  public String getCredentialsId() {
+    return credentialsId;
+  }
+
+  @DataBoundSetter
+  public void setCredentialsId(@CheckForNull String credentialsId) {
+    this.credentialsId = credentialsId;
+  }
+
+  @NonNull
+  public UsernamePasswordCredentials getCredentials() throws NoSuchElementException {
+    return getCredentials(credentialsId);
+  }
+
+  @NonNull
+  private UsernamePasswordCredentials getCredentials(String credentialsId) throws NoSuchElementException {
+    Optional<Credentials> optionalCredentials = SystemCredentialsProvider.getInstance()
+      .getCredentials()
+      .stream()
+      .filter(credentials -> (credentials instanceof UsernamePasswordCredentials) && ((IdCredentials) credentials).getId().equals(credentialsId))
+      .findAny();
+    return (UsernamePasswordCredentials) optionalCredentials.get();
+  }
+
+  @CheckForNull
+  public String getIndexPattern() {
+    return indexPattern;
+  }
+
+  @DataBoundSetter
+  public void setIndexPattern(@CheckForNull String indexPattern) {
+    this.indexPattern = indexPattern;
+  }
+
+  @RequirePOST
+  public FormValidation doCheckKibanaUrl(@QueryParameter("kibanaUrl") String url) {
+    if (StringUtils.isEmpty(url)) {
+      return FormValidation.ok();
     }
+    try {
+      new URL(url);
+    } catch (MalformedURLException e) {
+      return FormValidation.error(ERROR_MALFORMED_URL, e);
+    }
+    return FormValidation.ok();
+  }
+
+  @RequirePOST
+  public FormValidation doCheckInput(@QueryParameter("input") String uri) {
+    if (StringUtils.isEmpty(uri)) {
+      return FormValidation.warning("The Filebeat input is required.");
+    }
+    URI checkUri = URI.create(uri);
+    if (validSchemas.contains(checkUri.getScheme())) {
+      return FormValidation.ok();
+    }
+
+    return FormValidation.error("The Filebeat input URI is not valid.");
+  }
+
+  @RequirePOST
+  public FormValidation doCheckElasticsearchUrl(@QueryParameter("elasticsearchUrl") String url) {
+    if (StringUtils.isEmpty(url)) {
+      return FormValidation.warning("The Elasticsearch URL is required.");
+    }
+    try {
+      new URL(url);
+    } catch (MalformedURLException e) {
+      return FormValidation.error(ERROR_MALFORMED_URL, e);
+    }
+    return FormValidation.ok();
+  }
+
+  @RequirePOST
+  public ListBoxModel doFillCredentialsIdItems(Item context,
+                                               @QueryParameter String credentialsId) {
+    if (context == null && !Jenkins.get().hasPermission(Jenkins.ADMINISTER) ||
+      context != null && !context.hasPermission(context.CONFIGURE)) {
+      return new StandardListBoxModel();
+    }
+
+    return new StandardListBoxModel().includeEmptyValue()
+      .includeAs(ACL.SYSTEM, context, StandardUsernameCredentials.class)
+      .includeCurrentValue(credentialsId);
+  }
+
+  @RequirePOST
+  public FormValidation doCheckCredentialsId(Item context,
+                                             @QueryParameter String credentialsId) {
+    if (context == null && !Jenkins.get().hasPermission(Jenkins.ADMINISTER) ||
+      context != null && !context.hasPermission(context.CONFIGURE)) {
+      return FormValidation.ok();
+    }
+
+    try {
+      getCredentials(credentialsId);
+    } catch (NoSuchElementException e) {
+      return FormValidation.warning("The credentials are not valid.");
+    }
+    return FormValidation.ok();
+  }
+
+  @RequirePOST
+  public FormValidation doCheckIndexPattern(Item context,
+                                             @QueryParameter String indexPattern) {
+    if (StringUtils.isEmpty(indexPattern)) {
+      return FormValidation.warning("The Filebeat index pattern is required.");
+    }
+    return FormValidation.ok();
+  }
+
+  @RequirePOST
+  public FormValidation doValidate(@QueryParameter String elasticsearchUrl,
+                          @QueryParameter String credentialsId, @QueryParameter String indexPattern) {
+    try {
+      final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+      UsernamePasswordCredentials jenkinsCredentials = getCredentials(credentialsId);
+      org.apache.http.auth.UsernamePasswordCredentials credentials =
+        new org.apache.http.auth.UsernamePasswordCredentials(jenkinsCredentials.getUsername(), jenkinsCredentials.getPassword().getPlainText());
+      credentialsProvider.setCredentials(AuthScope.ANY, credentials);
+
+      RestClientBuilder builder = RestClient.builder(HttpHost.create(elasticsearchUrl));
+      builder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+        @Override
+        public HttpAsyncClientBuilder customizeHttpClient(
+          HttpAsyncClientBuilder httpClientBuilder) {
+          return httpClientBuilder
+            .setDefaultCredentialsProvider(credentialsProvider);
+        }
+      });
+
+      try (RestHighLevelClient client = new RestHighLevelClient(builder)) {
+        GetIndexRequest request = new GetIndexRequest(indexPattern);
+        client.indices().exists(request, RequestOptions.DEFAULT);
+      }
+    } catch(NoSuchElementException e){
+      return FormValidation.error("Invalid credentials");
+    } catch(IllegalArgumentException e){
+      return FormValidation.error("Invalid Elasticsearch host");
+    } catch (IOException e) {
+      return FormValidation.error("Unable to connect.");
+    }
+    return FormValidation.ok("success");
+  }
+
 }
