4d81a2fa139688f7af4071e4fe22d56d673a9374
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-07-05 14:08:14 +0200

    fix: use a local Opentelemetry logs service for tests

--- src/test/java/io/jenkins/plugins/elasticstacklogs/OpentelemetryLogsInputTest.java
+++ src/test/java/io/jenkins/plugins/elasticstacklogs/OpentelemetryLogsInputTest.java
@@ -3,7 +3,16 @@ package io.jenkins.plugins.elasticstacklogs;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
 import io.jenkins.plugins.elasticstacklogs.input.OpentelemetryLogsInput;
+import io.jenkins.plugins.elasticstacklogs.opentelemetry.TestLogExporter;
+import io.opentelemetry.proto.collector.logs.v1.ExportLogsServiceRequest;
+import io.opentelemetry.proto.collector.logs.v1.ExportLogsServiceResponse;
+import io.opentelemetry.proto.collector.logs.v1.LogsServiceGrpc;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -12,31 +21,62 @@ import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
 public class OpentelemetryLogsInputTest {
-  private static final File workdir = new File("/tmp");
+  private static final Logger LOGGER = Logger.getLogger(OpentelemetryLogsInputTest.class.getName());
 
+  private static final File workdir = new File("/tmp");
+  private Server server;
+
+  private class LogsService extends LogsServiceGrpc.LogsServiceImplBase {
+
+    @Override
+    public void export(ExportLogsServiceRequest request, StreamObserver<ExportLogsServiceResponse> responseObserver) {
+      LOGGER.info(request.toString());
+      responseObserver.onNext(ExportLogsServiceResponse.newBuilder().build());
+      responseObserver.onCompleted();
+    }
+  }
+  /* FIXME enable logs support on opentelemetry-collector
   @Rule
-  public GenericContainer otelCollector = new GenericContainer("otel/opentelemetry-collector:latest")
+  public GenericContainer otelCollector = new GenericContainer("otel/opentelemetry-collector-dev:latest")
     .withClasspathResourceMapping("otel-collector.yml", "/otel-collector.yml", BindMode.READ_ONLY)
     .withFileSystemBind(workdir.getAbsolutePath(), "/tmp", BindMode.READ_WRITE)
-    .withCommand("--config /otel-collector.yml")
+    .withCommand("--config /otel-collector.yml --log-level DEBUG --log-profile dev ")
     .withStartupTimeout(Duration.ofMinutes(1));
-
+*/
   @BeforeClass
   public static void requiresDocker() {
     assumeTrue(DockerClientFactory.instance().isDockerAvailable());
   }
 
   @Before
-  public void setUp() {
+  public void setUp() throws IOException {
+    server = ServerBuilder.forPort(4317).addService(new LogsService())
+                          .build();
+    server.start();
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        try {
+          if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+          }
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        System.err.println("*** server shut down");
+      }
+    });
   }
 
   @Test
   public void testLog() throws IOException, InterruptedException {
-    OpentelemetryLogsInput input = new OpentelemetryLogsInput("127.0.0.1", otelCollector.getMappedPort(4317));
+    //OpentelemetryLogsInput input = new OpentelemetryLogsInput("127.0.0.1", otelCollector.getMappedPort(4317));
+    OpentelemetryLogsInput input = new OpentelemetryLogsInput("127.0.0.1", 4317);
     input.write("foo");
     Thread.sleep(5000);
     assertEquals(1, input.getCount());
